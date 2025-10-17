329b97c643939642cdffc805d3e34a139d1ac302
fkauanGIT <kauanuniversity@gmail.com>
2025-10-17 10:58:54 -0300

    feat: adicionando personagem principal + movimentos

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -11,7 +11,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize  = 16; // 16x16 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale; // 48x48 tile
+    public final int tileSize = originalTileSize * scale; // 48x48 tile
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol; // 768 px
@@ -23,11 +23,6 @@ public class GamePanel extends JPanel implements Runnable{
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     Player player = new Player(this, keyH);
-
-    // Set player´s default position
-    int playerX = 100;
-    int player_Y = 100;
-    int playerSpeed = 4;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -69,24 +64,14 @@ public class GamePanel extends JPanel implements Runnable{
         }
     }
     public void update() {
-        if(keyH.upPressed == true) {
-            player_Y -= playerSpeed;
-        } else if(keyH.downPressed == true) {
-            player_Y += playerSpeed;
-        } else if(keyH.leftPressed == true) {
-            playerX -= playerSpeed;
-        } else if(keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        }
+        player.update();
     }
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Graphics g2  = (Graphics2D)g;
 
-        g2.setColor(Color.WHITE);
-
-        g2.fillRect(playerX, player_Y, tileSize, tileSize);
+        player.draw(g2);
 
         g2.dispose();
     }
